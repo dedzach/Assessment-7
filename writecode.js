f54d38ad2f48b06498dc9daf43b97627c8052b94
Zachary Moh <zacharymoh11@gmail.com>
2021-09-17 10:43:38 -0500

    update

--- writecode.js
+++ writecode.js
@@ -4,7 +4,7 @@ function uniqueChars(str) {
     for (let i = 0; i < str.length; i++) {
         if(phrase.hasUniqueChars(str['Zach'])) {
             return true;
-        } else { return false; } 
+        } else { return false; }    
     }
     return phrase
 }
@@ -12,4 +12,6 @@ function uniqueChars(str) {
 console.log(uniqueChars('Zach is amazing!'))
 
 // runtime of this code should be O(n) because we don't know how many strings containing Zach the phrase will have.
-// If we're soley basing it off of what's in the console.log it would be a O(1)
+// If we're soley basing it off of what's in the console.log it would be a O(1).
+
+// code does not run T^T
