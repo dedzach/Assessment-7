63ead3017655c664cb69258ed15fbc27996386ae
Zachary Moh <zacharymoh11@gmail.com>
2021-09-17 10:35:51 -0500

    first commit

---
+++ writecode.js
@@ -0,0 +1,15 @@
+// Unique Characters 
+function uniqueChars(str) {
+    let phrase = {};
+    for (let i = 0; i < str.length; i++) {
+        if(phrase.hasUniqueChars(str['Zach'])) {
+            return true;
+        } else { return false; } 
+    }
+    return phrase
+}
+
+console.log(uniqueChars('Zach is amazing!'))
+
+// runtime of this code should be O(n) because we don't know how many strings containing Zach the phrase will have.
+// If we're soley basing it off of what's in the console.log it would be a O(1)
